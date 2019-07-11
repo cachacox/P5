ea87108a234ef300eef6558bc90f5c95161b26a6
xcachaconx <cchacon_25@hotmail.com>
2019-07-11 00:18:33 -0600

    index

---
+++ P5Parcial/P5Parcial/Models/usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P5Parcial.Models
+{
+    public class usuario
+    {
+        public string correo { get; set; }
+        public string contrasena { get; set; }
+    }
+}
